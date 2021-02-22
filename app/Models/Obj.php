3e5637f2e113eeb8b1eaf3f2cdf928f95a5ef7ea
Luteranya Nyange <dieume.dev@gmail.com>
2021-02-22 14:49:03 +0300

    File and folder models and migrations

---
+++ app/Models/Obj.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Obj extends Model
+{
+    use HasFactory;
+
+    public $table = "objects";
+
+    protected $fillable = [
+        "parent_id",
+    ];
+
+    public static function booted()
+    {
+        static::creating(function ($model) {
+            $model->uuid = Str::uuid();
+        });
+    }
+
+
+    public function objectable()
+    {
+        return $this->morphTo();
+    }
+}
